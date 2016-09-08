ef4fc10d2925b30bc9d798f7fd35a09bd236c10f
Jesus Saravia <jesusdsaravia@gmail.com>
2016-09-08 17:59:41 -0500

    Primeras cuatro operaciones lista

---
+++ src/clases/Fraccionario.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Jesus
+ */
+public class Fraccionario {
+  private int numerador;
+  private int denominador;
+  
+  public Fraccionario(int numerador, int denominador){
+  this.numerador=numerador;
+  this.denominador=denominador;
+  }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+    public Fraccionario sumar(Fraccionario f2){
+       Fraccionario f;
+        int num,den;
+        
+        num = this.numerador * f2.denominador + this.denominador * f2.numerador;
+        den = this.denominador * f2.denominador;
+        f = new Fraccionario(num,den);
+        return f;  
+    }
+    public Fraccionario restar(Fraccionario f2){
+       Fraccionario f;
+        int num,den;
+        
+        num = this.numerador * f2.denominador - this.denominador * f2.numerador;
+        den = this.denominador * f2.denominador;
+        f = new Fraccionario(num,den);
+        return f;  
+  
+    }
+    public Fraccionario multiplicar(Fraccionario f2){
+       Fraccionario f = null;
+       int num,den;
+        num = this.numerador * f2.numerador;
+       den = this.denominador * f2.denominador;
+       return f;
+    }
+
+}
